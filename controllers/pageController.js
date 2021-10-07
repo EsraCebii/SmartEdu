9b481a2d8a30072b6e992209ad4c2d6da4d2ba9f
Esra Cebi <esracebi.81@gmail.com>
2021-10-08 00:53:43 +0300

    giriş sayfası

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -11,6 +11,11 @@ exports.getAboutPage = (req, res) => {
 };
 exports.getRegisterPage = (req, res) => {
     res.status(200).render('register', {
-        page_name: "register"
+        page_name: "register",
+    });
+};
+exports.getLoginPage = (req, res) => {
+    res.status(200).render('login', {
+        page_name: "login",
     });
 };
