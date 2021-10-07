8340a8b567728493c46005ff62156999a721c740
Esra Cebi <esracebi.81@gmail.com>
2021-10-07 19:54:16 +0300

    kursları listelemek

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
+
+// Connect DB
+mongoose.connect('mongodb://localhost/smartedu-db').then(()=> {
+    console.log('DB connected successfuly');
+});
 
 // Template Engine
 app.set("view engine", "ejs");
 
 //Middlewares
 app.use(express.static("public"));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 //Routes
 app.use('/', pageRoute);
-// app.use('/courses', courseRoute )
+app.use('/courses', courseRoute)
 
 
 const port = 3000;
