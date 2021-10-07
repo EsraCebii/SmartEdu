55b18509f421e576e3c4630c635efcaff43b8adf
Esra Cebi <esracebi.81@gmail.com>
2021-10-08 01:38:38 +0300

    session -1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -9,22 +10,41 @@ const app = express();
 
 // Connect DB
 mongoose.connect('mongodb://localhost/smartedu-db').then(()=> {
-    console.log('DB connected successfuly');
+    console.log('DB connected Successfully');
 });
 
 // Template Engine
 app.set("view engine", "ejs");
 
+//Global Variable
+global.userIN = null;
+
 //Middlewares
+
 app.use(express.static("public"));
 app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); // for parsing application/
+app.use(
+    session({
+      secret: 'my_keyboard_cat', // Buradaki texti değiştireceğiz.
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
+app.use(
+    session({
+      secret: 'my_keyboard_cat',
+      resave: false,
+      saveUninitialized: true,
+    })
+);
 
 //Routes
 app.use('/', pageRoute);
 app.use('/courses', courseRoute)
 app.use('/categories', categoryRoute)
 app.use('/users', userRoute)
+
 
 const port = 3000;
 app.listen(port, () => {
