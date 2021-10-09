d88c01db6983b176df11c19479dcc734b128a5e4
Esra Cebi <esracebi.81@gmail.com>
2021-10-09 21:26:20 +0300

    iletişim sayfası ve mail göndermek

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require("nodemailer");
+
 exports.getIndexPage = (req, res) => {
     console.log(req.session.userID)
     res.status(200).render('index', {
@@ -19,4 +21,42 @@ exports.getLoginPage = (req, res) => {
     res.status(200).render('login', {
         page_name: "login",
     });
+};
+exports.getContactPage = (req, res) => {
+    res.status(200).render('contact', {
+        page_name: "contact",
+    });
+};
+exports.sendEmail = async (req, res) => {
+    const outputMessage = `
+        <h1>Mail Details</h1>
+            <ul>
+                <li>Name: ${req.body.name} </li>
+                <li>Email: ${req.body.email} </li>
+            </ul>
+        <h1>Message Details</h1>
+        <p> ${req.body.message} </p>
+    `
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: "esracebi.81@gmail.com" , // gmail account
+          pass: "rohuihsdokuyyrjj", // gmail password
+        },
+      });
+    
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: '"Smart EDU Contact Form" <esracebi.81@gmail.com>', // sender address
+        to: "dumbosprnts@gmail.com", // list of receivers
+        subject: "Smart EDU Contact Form New Message", // Subject line
+        html: outputMessage, // html body
+      });
+    
+      console.log("Message sent: %s", info.messageId);
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+      res.status(200).redirect('contact');
 };
